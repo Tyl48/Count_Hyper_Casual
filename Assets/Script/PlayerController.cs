b8e3758b121fc2a6809bf5d19761cfa89be6c0f3
Thinhdeptrai <Nguyentruongthinh.9876@gmail.com>
2025-05-14 14:55:00 +0700

    add speed for player

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    [Header("Settings")]
+    [SerializeField] private float moveSpeed = 0;
+    [SerializeField] private float roadWidth = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
